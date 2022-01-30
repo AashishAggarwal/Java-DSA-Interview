20c9215c258d60aee39d14e35d44e3b96fce8f7c
Aashish <aggarwal.aashish13@gmail.com>
2022-01-30 16:04:18 +0530

    Add files

---
+++ Arrays/CyclicSort.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+public class CyclicSort {
+
+    public static void sort(int[] nums) {
+        // TODO: Write your code here
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] != i + 1) {
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] { 3, 1, 5, 4, 2 };
+        CyclicSort.sort(arr);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+
+        arr = new int[] { 2, 6, 4, 3, 1, 5 };
+        CyclicSort.sort(arr);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+
+        arr = new int[] { 1, 5, 6, 4, 3, 2 };
+        CyclicSort.sort(arr);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+    }
+}
